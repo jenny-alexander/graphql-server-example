910f8d06fdec2ec86a2a8ab0fb2ac6b596eef360
Jennifer Alexander <Jennifer.Alexander@thrivent.com>
2024-04-15 21:17:08 -0500

    Add mutations to add, delete and update a game

--- schema.js
+++ schema.js
@@ -4,18 +4,21 @@ export const typeDefs = `#graphql
         id: ID!
         title: String!
         platform: [String!]!
+        reviews: [Review!]
     }
     type Review {
         id: ID!
         rating: Int!
         content: String!
+        author: Author
+        game: Game
     }
     type Author {
         id: ID!,
         name: String!
         verified: Boolean!
+        reviews: [Review]
     }
-
     type Query {
         reviews: [Review]
         review(id: ID!): Review
@@ -23,6 +26,19 @@ export const typeDefs = `#graphql
         game(id: ID!): Game
         authors: [Author]
         author(id: ID!): Author
+    }
+    type Mutation {
+        deleteGame(id: ID!): [Game]
+        addGame(game: AddGameInput): Game
+        updateGame(id: ID!, edits: EditGameInput!): Game
+    }
+    input AddGameInput {
+        title: String!
+        platform: [String!]!
+    }
+    input EditGameInput {
+        title: String
+        platform: [String]!
     }
 `
 // Query --> entry point into the graph
